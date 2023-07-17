6d3eb518d6377f077ac1b273e3ce2ea5dcda6898
Aditya Girijala <aditya.girijala>
2023-07-17 22:45:22 +0530

    fixed error

--- terraform-files/c2-vnet-outputs.tf
+++ terraform-files/c2-vnet-outputs.tf
@@ -1,19 +1,19 @@
 output "Azure_Virtual_Network_Name" {
   description = "Azure_Virtual_Network_Name"
-  value       = azurerm_virtual_network.azure_vnet.name
+  value       = azurerm_virtual_network.az_vnet.name
 }
 
 output "Azure_Virtual_Network_ID" {
   description = "Azure_Virtual_Network_ID"
-  value       = azurerm_virtual_network.azure_vnet.id
+  value       = azurerm_virtual_network.az_vnet.id
 }
 
 output "Azure_Virtual_Network_Location" {
   description = "Azure_Virtual_Network_Location"
-  value       = azurerm_virtual_network.azure_vnet.location
+  value       = azurerm_virtual_network.az_vnet.location
 }
 
 output "Azure_Virtual_Network_Address_Space" {
   description = "Azure_Virtual_Network_Address_Space"
-  value       = azurerm_virtual_network.azure_vnet.address_space[*]
+  value       = azurerm_virtual_network.az_vnet.address_space[*]
 }
